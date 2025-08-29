444aa8fb5123bf82a2c88988fd57cd3449968c41
Alberdi Moreno <alberdimoreno0@gmail.com>
2025-08-29 04:47:12 +0000

    mejoras2

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,12 +1,12 @@
 import { createInstance } from 'i18next';
 import resourcesToBackend from 'i18next-resources-to-backend';
-import { initReactI18next } from 'react-i18next';
+// Eliminamos la importación de initReactI18next aquí, ya que no es para el lado del servidor.
 import { getOptions, languages } from './config';
 
 const initI18next = async (lang: string, ns: string | string[] = 'translation') => {
   const i18nInstance = createInstance();
   await i18nInstance
-    .use(initReactI18next)
+    // Eliminamos .use(initReactI18next) del lado del servidor.
     .use(resourcesToBackend((language: string, namespace: string) => import(`../../public/locales/${language}/${namespace}.json`)))
     .init(getOptions(lang, ns));
   return i18nInstance;
